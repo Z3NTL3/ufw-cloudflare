551a871a4b2e36fd36ed473d4c06e1eed7936eb9
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2023-04-17 10:56:35 +0200

    Update ufw.py

--- ufw.py
+++ ufw.py
@@ -3,9 +3,12 @@ import subprocess
 
 try:
     # clear first
-    subprocess.Popen("ufw reset", shell=True)
-    subprocess.Popen("ufw default deny incoming",
-                     shell=True)
+    p = subprocess.Popen("ufw reset", shell=True, stdin=subprocess.PIPE)
+    p.communicate(input='y\n'.encode())
+
+    p = subprocess.Popen("ufw default deny incoming",
+                         shell=True, stdin=subprocess.PIPE)
+    p.communicate(input='y\n'.encode())
 
     with httpx.Client(headers={"Cache-Control": "must-revalidate", "Content-Type": "text/plain"}) as client:
         ipv4s = client.get("https://www.cloudflare.com/ips-v4").text
@@ -15,15 +18,20 @@ try:
         ipv6s = ipv6s.strip().split("\n")
 
         for ipv4 in ipv4s:
-            subprocess.Popen(
-                f"ufw allow proto tcp from {ipv4} comment 'CF IP'", shell=True)
+            p = subprocess.Popen(
+                f"ufw allow proto tcp from {ipv4} comment 'CF IP'", shell=True, stdin=subprocess.PIPE)
+            p.communicate(input='y\n'.encode())
         for ipv6 in ipv6s:
-            subprocess.Popen(
-                f"ufw allow proto tcp from {ipv6} comment 'CF IP'", shell=True)
+            p = subprocess.Popen(
+                f"ufw allow proto tcp from {ipv6} comment 'CF IP'", shell=True, stdin=subprocess.PIPE)
+            p.communicate(input='y\n'.encode())
 
-        subprocess.Popen("ufw allow ssh",
-                         shell=True)
-        subprocess.Popen("ufw reload", shell=True)
-        subprocess.Popen("ufw enable", shell=True)
+        p = subprocess.Popen("ufw allow ssh",
+                             shell=True)
+        p.communicate(input='y\n'.encode())
+        p = subprocess.Popen("ufw reload", shell=True, stdin=subprocess.PIPE)
+        p.communicate(input='y\n'.encode())
+        p = subprocess.Popen("ufw enable", shell=True, stdin=subprocess.PIPE)
+        p.communicate(input='y\n'.encode())
 except:
     print("Failed")
